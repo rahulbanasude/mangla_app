db00ec578ce3107985b92ea13a3e9e2c113683dc
rahulbanasude <rahul.bansude2@gmail.com>
2016-07-18 03:30:57 +0530

    passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.5.0'
 
 set :application, 'mangla_app'
 set :repo_url, 'git@github.com:rahulbanasude/mangla_app.git'
-
+set :passenger_restart_with_touch, true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
