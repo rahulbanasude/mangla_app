c75656ec359ab8d06b6bac4627a7e0e5f4a69cc6
rahulbanasude <rahul.bansude2@gmail.com>
2016-07-23 03:43:44 +0530

    test 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,9 +29,9 @@ set :passenger_restart_with_touch, true
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
-
+set :bundle_binstubs, nil
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
