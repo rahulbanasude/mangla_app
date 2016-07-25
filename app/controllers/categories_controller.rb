86b9043506cbbc4ef07a4d2df6fd955d2c46b6fd
rahulbanasude <rahul.bansude2@gmail.com>
2016-07-25 14:05:09 +0530

    fixed bootstap js

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
+	def index
+	
+end
 def category_params
       params.require(:category).permit(:name)
     end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+		has_many :galleries
+	accepts_nested_attributes_for :galleries
 end
