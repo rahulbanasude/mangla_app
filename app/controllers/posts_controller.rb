c56a245dcde1c64473906ae1a7a9ecc5a970bb53
rahulbanasude <rahul.bansude2@gmail.com>
2017-01-05 00:11:58 +0530

    fixed blog page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.all
   end
 
   def show
   end
+
+
+  private
+  
+  def post_params
+      params.require(:post).permit(:title, :body, :image)
+    end
 end
